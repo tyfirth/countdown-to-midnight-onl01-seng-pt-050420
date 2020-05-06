9937a457ed6eab3448c687fbaa115c1d718015d4
Tyler Firth <tyfirth@gmail.com>
2020-05-06 22:35:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,12 @@ def countdown(n)
  return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(n)
+  
+  while n > 0 
+  puts "#{n} SECOND(S)"
+  sleep(0.5)
+  n -= 1
+ end
+ return "HAPPY NEW YEAR!"
+end
